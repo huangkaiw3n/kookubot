c274fbef5a40466bc46992895a236fe8a2c6f129
Huang Kaiwen <huangkaiw3n@gmail.com>
2021-04-23 12:35:47 +0800

    feat: update timeslot

--- ClimbCentral.js
+++ ClimbCentral.js
@@ -18,11 +18,11 @@ let lastCheckTimestamp = moment()
 let lastCapacityString = null
 
 // Notification message to send when there's a slot
-const NOTIFY_SLOT_MESSAGE = 'CC Stadium Thu Apr 15 8pm/8:50pm has slot! https://www.climbcentral.sg/timeslot/ccsh'
+const NOTIFY_SLOT_MESSAGE = 'CC Stadium Mon Apr 26 8pm/8:50pm has slot! https://www.climbcentral.sg/timeslot/ccsh'
 
 // Capacity String to search for in html response
-const TIME1 = 'Thu, April 15, 8 PM to  10:20 PM'
-const TIME2 = 'Thu, April 15, 8:50 PM to  10:50 PM'
+const TIME1 = 'Mon, April 26, 8 PM to  10:20 PM'
+const TIME2 = 'Mon, April 26, 8:50 PM to  10:50 PM'
 
 // The possible Availability responses when there is space
 const ONE_SPACE = '1 space'
